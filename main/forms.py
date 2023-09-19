685ef7eead48672adba34c9c38aedbdf6d7e68cf
nazwaikhsan <nazwa.arifah@ui.ac.id>
2023-09-19 15:11:57 +0700

    redo some stuffs because of error

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Item
-
-class dataBarang(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ['name', 'amount', 'description']
--- main/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    description = models.TextField()
