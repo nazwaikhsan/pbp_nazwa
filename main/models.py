54c8e12de9dff057a699a42e7a314ff3dcd511fc
nazwaikhsan <nazwa.arifah@ui.ac.id>
2023-09-26 16:47:22 +0700

    done tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -6,3 +7,4 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     date_added = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
